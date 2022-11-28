51f3dc3e3e5f2a15a8a993ea468cb1b6e8c67a90
Orlando N. Rodriguez <onovas05@gmail.com>
2022-11-27 21:21:42 -0500

    Initial Commit

---
+++ TODO/TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOApp.swift
+//  TODO
+//
+//  Created by Orlando on 11/27/22.
+//
+
+import SwiftUI
+
+@main
+struct TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
